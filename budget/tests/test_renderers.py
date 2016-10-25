37eaec7cade3339e00a6914bdc6142a12d705c49
Brett Lentz <blentz@redhat.com>
2016-10-25 15:33:43 -0400

    tighten up renderers tests

--- budget/tests/test_renderers.py
+++ budget/tests/test_renderers.py
@@ -3,8 +3,8 @@ from budget.renderers import CSVRenderer
 from pyramid import testing
 
 class TestCsvRenderer(unittest.TestCase):
-    data = { 'header' : ['x','y','z'],
-            'rows': [[1,2,3],[4,5,6],[7,8,9]] }
+    data = {'header' : ['x', 'y', 'z'],
+            'rows': [[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
 
     csv = "x,y,z\r\n1,2,3\r\n4,5,6\r\n7,8,9\r\n"
 
@@ -14,18 +14,28 @@ class TestCsvRenderer(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-    def _makeOne(self, info):
-        return CSVRenderer(info)
+    def runTest(self):
+        self.test_defaults()
+        self.test_with_content_type()
+        self.test_without_request()
 
-    def test_it(self):
+    def test_defaults(self):
+        ''' test that the defaults work and that they don't change the request
+        '''
         request = testing.DummyRequest()
-        renderer = self._makeOne('foo')(self.data,{'request':request})
-        self.assertEqual(renderer, self.csv)
+        renderer = CSVRenderer('foo')(self.data, {'request':request})
         self.assertEqual(request.response.content_type, 'text/csv')
+        self.assertEqual(renderer, self.csv)
 
-    def test_with_request_content_type_set(self):
+    def test_with_content_type(self):
+        ''' test that we can change our content-type in the request '''
         request = testing.DummyRequest()
         request.response.content_type = 'text/mishmash'
-        renderer = self._makeOne('foo')(self.data,{'request':request})
+        renderer = CSVRenderer('foo')(self.data, {'request':request})
         self.assertEqual(request.response.content_type, 'text/mishmash')
+        self.assertEqual(renderer, self.csv)
 
+    def test_without_request(self):
+        ''' test null inputs still produces the expected csv '''
+        renderer = CSVRenderer()(self.data, {'request':None})
+        self.assertEqual(renderer, self.csv)
