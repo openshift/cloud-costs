ea95d07d01ab11920dce2f6afa60d74b97654269
Brett Lentz <blentz@redhat.com>
2016-10-25 15:14:39 -0400

    clean up pylint issues in renderers.py

--- budget/renderers.py
+++ budget/renderers.py
@@ -1,3 +1,5 @@
+''' Custom renderers used in the creation of views '''
+
 import csv
 
 try:
@@ -6,25 +8,28 @@ except ImportError:
     from io import StringIO # python 3
 
 class CSVRenderer(object):
-   def __init__(self, info):
-      pass
+    ''' Render data as a CSV '''
+    def __init__(self, info):
+        pass
 
-   def __call__(self, value, system):
-      """ Returns a plain CSV-encoded string with content-type
-      ``text/csv``. The content-type may be overridden by
-      setting ``request.response.content_type``."""
+    def __call__(self, value, system):
+        """ Returns a plain CSV-encoded string with content-type
+        ``text/csv``. The content-type may be overridden by
+        setting ``request.response.content_type``."""
 
-      request = system.get('request')
-      if request is not None:
-         response = request.response
-         ct = response.content_type
-         if ct == response.default_content_type:
-            response.content_type = 'text/csv'
+        request = system.get('request')
+        if request is not None:
+            response = request.response
+            if response.content_type == response.default_content_type:
+                response.content_type = 'text/csv'
 
-      fout = StringIO()
-      writer = csv.writer(fout, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+        fout = StringIO()
+        writer = csv.writer(fout,
+                            delimiter=',',
+                            quotechar=',',
+                            quoting=csv.QUOTE_MINIMAL)
 
-      writer.writerow(value.get('header', []))
-      writer.writerows(value.get('rows', []))
+        writer.writerow(value.get('header', []))
+        writer.writerows(value.get('rows', []))
 
-      return fout.getvalue()
+        return fout.getvalue()
